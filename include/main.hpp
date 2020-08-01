1a66baac0aeb79976caef2f89de28d82df7b7294
Theo <theo.cousinet@epitech.eu>
2020-08-01 05:27:34 +0200

    d

---
+++ include/main.hpp
@@ -0,0 +1,14 @@
+/*
+** GODOT PROJECT, 2020
+** bluetooth module
+** File description:
+** main.hpp
+*/
+
+#ifndef _MY_MAIN_HPP
+#define _MY_MAIN_HPP
+
+#include <iostream>
+#include <vector>
+
+#endif
