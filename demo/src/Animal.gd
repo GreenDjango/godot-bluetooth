173be25d089efd578ed91ee33c530b85b180a6f2
Theo <theo.cousinet@epitech.eu>
2022-05-15 00:50:16 +0200

    UPDATE: godot demo project

---
+++ demo/src/Animal.gd
@@ -0,0 +1,65 @@
+class_name Animal
+extends Node2D
+
+var id : String
+var device_name : String
+var device_address : String
+var device_type : String
+const animals = ['snake', 'pigeon']
+var animal_id := 0
+var to_position := .0
+var to_direction := 0
+
+onready var sprite = $AnimatedSprite
+onready var label = $Front/Label
+onready var panel = $Front/Panel
+onready var description = $Front/Panel/VBoxContainer/Description
+
+func setup(_device_name: String, _device_address: String, _device_type: String):
+	device_name = _device_name
+	device_address = _device_address
+	device_type = _device_type
+	id = generate_id(device_address, device_name)
+	match device_type:
+		'phone':
+			animal_id = 1
+
+func _ready():
+	panel.visible = false
+
+	label.text = device_name if device_name else "???"
+	if (label.text.length() > 10):
+		label.text = label.text.substr(0, 12) + "."
+
+	description.text = "Name: {na}\nAddress: {ad}\nType: {ty}\n".format(
+		{"ad":device_address,"na":device_name,"ty":device_type}
+	)
+	
+	var frame_length = sprite.frames.get_frame_count(animals[animal_id] + "_walk")
+	if (frame_length > 0):
+		sprite.frame = randi() % frame_length
+	sprite.play(animals[animal_id] + "_walk")
+
+	position.x = position.x + rand_range(-760, 760)
+	position.y = position.y + rand_range(-20, 25)
+	_change_direction()
+
+func _process(delta):
+	if (abs(position.x - to_position) <= 1):
+		_change_direction()
+	else:
+		position.x += to_direction * (15 * delta)
+
+func _change_direction():
+	to_position = rand_range(-760, 760)
+	to_direction = int(sign(to_position - position.x))
+	sprite.flip_h = true if to_direction < 0 else false
+
+static func generate_id(address: String, name: String):
+	return address + '/' + name
+
+func _on_Area2D_mouse_entered():
+	panel.visible = true
+
+func _on_Area2D_mouse_exited():
+	panel.visible = false
--- demo/src/Main.gd
+++ demo/src/Main.gd
@@ -1,22 +1,50 @@
-extends Control
+extends Node2D
+
+var time_elapse := 0.0
+const animal_scene := preload("res://src/Animal.tscn")
+
+onready var animals_node := $World/Animals
+onready var bluetooth := Bluetooth.new()
 
 func _ready():
-	$GridContainer/CheckButton.pressed = $Bluetooth.powered
-	$GridContainer/DeviceName.text += $Bluetooth.get_device_name() + ' (' + $Bluetooth.get_alias() +')'
-	$GridContainer/Address.text += $Bluetooth.get_address()
+	randomize()
+	refresh_devices()
 
-func _process(_delta):
-	if $Timer.is_stopped():
-		$GridContainer/CheckButton.pressed = $Bluetooth.powered
-		$Timer.start()
+func _process(delta):
+	time_elapse += delta
 
-func _on_CheckButton_toggled(button_pressed):
-	$Bluetooth.powered = button_pressed
-	
-func _on_CheckButton2_pressed():
-	# var res := {'a':'b','c':'d'}
-	$GridContainer/CheckButton2/RichTextLabel.text = ""
-	var dev = $Bluetooth.list_devices()
-	for i in dev:
-		# prints(i, res[i])
-		$GridContainer/CheckButton2/RichTextLabel.bbcode_text += "[u]{key}[/u]: {value}\n".format({"key":i,"value":dev[i]})
+	if (time_elapse > 5):
+		time_elapse = 0
+		refresh_devices()
+
+func refresh_devices():
+	var animals := animals_node.get_children()
+	var devices_list := bluetooth.list_devices()
+
+	for device in devices_list:
+		var exist := false
+		var id = Animal.generate_id(device.address, device.name)
+
+		for current_animal in animals:
+			if (current_animal.id == id):
+				exist = true
+				break
+		if (exist):
+			continue
+
+		var animal : Animal = animal_scene.instance()
+		animal.setup(device.name, device.address, device.icon)
+		animals_node.add_child(animal, true)
+		
+	for current_animal in animals:
+		var to_remove := true
+
+		for device in devices_list:
+			var id = Animal.generate_id(device.address, device.name)
+			if (current_animal.id == id):
+				to_remove = false
+				break
+		if (!to_remove):
+			continue
+		current_animal.queue_free()
+
