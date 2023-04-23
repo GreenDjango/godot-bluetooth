2323354b083b7f55010b4e224ad12a0b0e98517a
Theo <theo.cousinet@epitech.eu>
2023-04-23 18:14:39 +0200

    UPDATE: clang format

--- src/godot_binding/register_types.cpp
+++ src/godot_binding/register_types.cpp
@@ -8,39 +8,38 @@
 \*****************************************************************************/
 
 #include "register_types.h"
-
 #include "ble/GDSimpleBLE.h"
 
 #include <gdextension_interface.h>
-#include <godot_cpp/core/defs.hpp>
 #include <godot_cpp/core/class_db.hpp>
+#include <godot_cpp/core/defs.hpp>
 #include <godot_cpp/godot.hpp>
 
 using namespace godot;
 
-void initialize_example_module(ModuleInitializationLevel p_level) {
-    if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
-        return;
-    }
+void initialize_blueplug_module(ModuleInitializationLevel p_level) {
+	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
+		return;
+	}
 
-    ClassDB::register_class<GDSimpleBLE>();
+	ClassDB::register_class<GDSimpleBLE>();
 }
 
-void uninitialize_example_module(ModuleInitializationLevel p_level) {
-    if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
-        return;
-    }
+void uninitialize_blueplug_module(ModuleInitializationLevel p_level) {
+	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
+		return;
+	}
 }
 
 extern "C" {
 // Initialization.
 GDExtensionBool GDE_EXPORT blueplug_library_init(const GDExtensionInterface *p_interface, const GDExtensionClassLibraryPtr p_library, GDExtensionInitialization *r_initialization) {
-    godot::GDExtensionBinding::InitObject init_obj(p_interface, p_library, r_initialization);
+	godot::GDExtensionBinding::InitObject init_obj(p_interface, p_library, r_initialization);
 
-    init_obj.register_initializer(initialize_example_module);
-    init_obj.register_terminator(uninitialize_example_module);
-    init_obj.set_minimum_library_initialization_level(MODULE_INITIALIZATION_LEVEL_SCENE);
+	init_obj.register_initializer(initialize_blueplug_module);
+	init_obj.register_terminator(uninitialize_blueplug_module);
+	init_obj.set_minimum_library_initialization_level(MODULE_INITIALIZATION_LEVEL_SCENE);
 
-    return init_obj.init();
+	return init_obj.init();
 }
 }
