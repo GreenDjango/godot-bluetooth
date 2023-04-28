5acfda54db95d985954418c0b7b33570e6f729eb
Theo <theo.cousinet@epitech.eu>
2023-04-28 11:11:01 +0200

    UPDATE: demo

--- demo/scripts/main.gd
+++ demo/scripts/main.gd
@@ -1,18 +1,29 @@
 extends Control
 
-var blueplug = BluePlug.new()
-# var address := "TO_CHANGE"
+var blueplug := BluePlug.new()
+var address := "TO_CHANGE"
 
 func _ready():
+	# test()
+	pass
+
+func test():
 	blueplug.init_adapter(blueplug.adapters()[0])
-	
+
 	blueplug.connect("peripheral_found", _on_peripheral_found)
-	
+
 	blueplug.start_scan()
 	await get_tree().create_timer(5).timeout
 	blueplug.stop_scan()
-	# print(blueplug.connect_peripheral(address))
-	# print(blueplug.peripheral_services(address))
+	blueplug.connect_peripheral(address)
+	print(blueplug.is_peripheral_connected(address))
+	print(blueplug.peripheral_services(address))
 
 func _on_peripheral_found(p_address: String):
-	prints(p_address, blueplug.peripheral_identifier(p_address), blueplug.peripheral_identifier(p_address))
+	prints(p_address, blueplug.peripheral_identifier(p_address))
+
+func _on_animal_demo_pressed():
+	get_tree().change_scene_to_file("res://scenes/animal_demo/main.tscn")
+
+func _on_multi_demo_pressed():
+	get_tree().change_scene_to_file("res://scenes/multiplayer/main.tscn")
